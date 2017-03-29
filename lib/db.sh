9cc0b7076d701ef6e905e66d1099d35dcbf63d04
Rose <rosegr01@bctlpedata12.risk.regn.net>
2017-03-29 11:56:13 -0400

    Finished db.sh preparing to write unit test script

--- lib/db.sh
+++ lib/db.sh
@@ -22,7 +22,6 @@ declare -A DML=(
 
 declare -a db_rows
 declare -a db_headers
-#declare db_select_regex='/^select\s+(\w|\*)+(,\s+\w+)*\s+from\s(?!where).+$/i'
 
 # Verifies that the db (the db provided via the param, or the one defined in contants.sh) file exists.
 # If the existants of the db files is verified then the `DB` variable is set, else an error is thrown.
@@ -41,15 +40,15 @@ db_init() {
 # Executes the provided select statement against the DB.
 # @arg:<string> - The 'SELECT' statement to be executed.
 db_do_query() {
-	db_do_chk ${@}
+	db_do_chk "${@}"
 	
 	local rslt
 
 	#[[ $( echo ${1} | perl -e 'print <STDIN> =~ @ARGV ? 0 : 1' ${db_select_regex} ) -eq 1 ]] && throw "InvalidArgument" "The provided query is improperly formated: ${1}"
 	
-	local qry=$(echo ${1} | grep -P -i -o '^select')
+	local qry=$(echo ${1} | grep -P -i -o '^select') || throw "InvalidArgument" "The provided query is not supported by the 'prct.db' module: ${1}"
 
-	[[ (${qry}) && $( echo ${1} | grep -P -i -q "${DML[${qry}]}" ) ]] || throw "InvalidArgument" "The provided query is improperly formated: ${1}"
+	$( echo ${1} | grep -P -i -q "${DML[${qry}]}" ) || throw "InvalidArgument" "The provided query is improperly formated: ${1}"
 
 	rslt=$(sqlite3 -separator ',' ${DB} "${1}" 2>&1) || throw "DatabaseException" "Query (${1}) failed with the following error: ${rslt}" 	
 	
@@ -60,13 +59,13 @@ db_do_query() {
 	return 0
 }
 
-
+# Executes the provided dml statement againt the DB.
 # @arg:<string> - The 'DML' statement to be executed.
 db_do_dml() {
 	db_do_chk "${@}"
 
 	local rslt 
-	local dml=$(echo ${1} | grep -P -i -o '^(insert|update|delete|merge)')
+	local dml=$(echo ${1} | grep -P -i -o '^(insert|update|delete)')
 	
 	[[ (${dml}) && (${DML[${dml}]}) ]] || throw "InvalidArgument" "The provided dml is not supported by the 'prct.db' module: ${1}"
 
@@ -80,7 +79,6 @@ db_do_dml() {
 # Check to ensure that the variable DB is set and the correct number of args
 # have been passed to the calling function.
 # @private
-# @static
 # @arg:<array> - The parameters passed to the calling function.
 db_do_chk() {
 	[[ ( ${#@} -gt 1 ) || ( ${#@} -lt 1 ) ]] && throw "InvalidArgument" "Invalid number of arguments provided"
@@ -100,7 +98,6 @@ db_set_rows() {
 
 # Set the variable db_headers to the headers 
 # @private
-# @static
 # @arg:<string> - The 'SELECT' statement to be parsed.
 db_set_headers() {
 	if $(echo "${1}" | grep -P -q -i 'select\s+\*\s+from\s(?!where)'); then
@@ -112,22 +109,37 @@ db_set_headers() {
 	fi
 }
 
+# Returns to the caller the contents of db_rows
+# @public
+# return:<string> - Space delimited string.
+db_get_rows() {
+	echo ${db_rows[@]}
+}
+
+# Returns to the caller the content of db_headers
+# @public
+# return:<string> - Space delimited string
+db_get_headers() {
+	echo ${db_headers[@]}
+}
+
 # Parsed from the provided 'SELECT' query the table that is being queried.
-# @arg:<string> - The 'SELECT' statement in which the table name needs to b parsed from.
 # @private
 # @static
+# @arg:<string> - The 'SELECT' statement in which the table name needs to b parsed from.
+# @return:<string> - The name of the table included in the select statment.
 db_get_table() {
 	echo $(echo ${1} | grep -P -i -o 'from.+' | awk '{print $2}')
 }
 
+db_init ${DB_DIR}'/../db/prct.db'
 #db_init ${DB_DIR}'/../db/fake.db'
 #db_get_table "SELECT * FROM table"
 #db_get_table "SELECT * FROM table WHERE x=y"
-db_init ${DB_DIR}'/../db/prct.db'
 #db_do_query "SELECT a, b, c FROM table WHERE x=y"
 #db_do_query 'SELECT * FROM datasets WHERE dataset_id IN (1, 2)'
 #db_do_query 'SELECT dataset_id, dataset_name, dataset_string FROM datasets WHERE dataset_id IN (1,2)'
 #db_do_dml 'INSERT INTO TABLE_NAME VALUES (value1, value2, value3, value3)' 'blah'
 #db_do_dml 'PUT INTO TABLE_NAME VALUES (value1, value2, value3, value3)'
 #db_do_dml 'INSERT INTO TABLE_NAME (value1, value2, value3, value3)'
-db_do_dml 'INSERT INTO TABLE_NAME VALUES (value1, value2, value3, value3)'
+#db_do_dml 'INSERT INTO TABLE_NAME VALUES (value1, value2, value3, value3)'
